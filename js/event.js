1bb36d577e6f4d5f41a2e7df9877bd61b4940e47
Refat Tamanna Ringky <95683348+RefatRingky@users.noreply.github.com>
2022-02-08 13:36:43 +0600

    first commit

---
+++ js/event.js
@@ -0,0 +1,35 @@
+  // js for red button//
+  function makeRed() {
+    document.body.style.backgroundColor = "Red";
+}
+
+//  js for blue button//
+const blueButton = document.getElementById("makeBlueButton");
+//   just set the name of the function without()
+blueButton.onclick = makeBlue;
+function makeBlue() {
+    document.body.style.backgroundColor = "Blue";
+}
+
+// for green button with using direct function
+const greenButton = document.getElementById("make-Green-Button");
+greenButton.onclick = function () {
+    document.body.style.backgroundColor = "Green";
+}
+
+// handle add eventlistener
+const goldenRod = document.getElementById("make-Gplden-Button");
+        goldenRod.addEventListener('click', makeGoldenButton);
+        function makeGoldenButton() {
+            document.body.style.backgroundColor = "GoldenRod";
+        }
+
+// handle add eventlistener with own function inside the addeventlisetener
+const hotPink = document.getElementById("hot-pink-button");
+hotPink.addEventListener('click', function() {
+    document.body.style.backgroundColor ='hotpink';
+})
+// adding direct addeventlistener
+document.getElementById("light-blue-button").addEventListener('click',function () {
+    document.body.style.backgroundColor ='lightblue';
+})
